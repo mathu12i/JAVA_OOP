6599f28b6356450a0db428483b074a56dced6dde
MATHURADA MEEPAN <mathurada.me@ku.th>
2025-02-11 13:51:07 +0700

    11/02/2568

--- src/TestShape.java
+++ src/TestShape.java
@@ -4,15 +4,16 @@ public class TestShape {
     public static void main(String[] args) {
         Circle circle = new Circle();
         circle.inputParameters();
+        //
         System.out.println("Circle radius: " + circle.getRadius());
         System.out.printf("Circle area: %.2f\n", circle.getArea());
         System.out.printf("Circle circumference: %.2f\n\n", circle.getCircumference());
-
+        //
         Triangle triangle = new Triangle();
         triangle.inputParameters();
         System.out.println("Triangle base: " + triangle.getBase() + " height: " + triangle.getHeight());
         System.out.printf("Triangle area: %.2f\n\n", triangle.getArea());
-
+        //
         Rectangle rectangle = new Rectangle();
         rectangle.inputParameters();
         System.out.println("Rectangle width: " + rectangle.getWidth() + " height: " + rectangle.getHeight());
